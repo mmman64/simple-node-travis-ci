fde05c6be1709dabf1c99a5c3ae7d9f22d3f9cd8
Marco-FI-SEI <mmc90@hotmail.co.uk>
2020-05-02 20:38:28 +0100

    📦 NEW: Add dummy env_var

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+const FAVOURITE_FOOD = process.env.FAVOURITE_FOOD;
+
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function main() {
-  while(true) {
-    console.log('Containers rule!');
+  while (true) {
+    console.log("Containers rule!");
     await sleep(5000);
   }
 }
+
+console.log(`My favourite food is ${FAVOURITE_FOOD}`);
 
 main();
